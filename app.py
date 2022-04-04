10ab8d27e09ab9d0901062c78ac9c0e1019c980a
Dikshant <dikshantlnaikxyz@gmail.com>
2022-04-04 17:34:25 +0530

    minor change

--- app.py
+++ app.py
@@ -20,17 +20,17 @@ def execute():
     print("--------------------------------")
     print("        SCRIPT STARTING ")
     print("--------------------------------")
-
-    thread =Thread(name="main",target=main.Main)
-    if thread.is_alive() == 0:
-        thread.start()
+    views.Main.Main()
+    # thread =Thread(name="main",target=main.Main)
+    # if thread.is_alive() == 0:
+    #     thread.start()
     return "heh"
 @app.route("/execute2")
 def execute2():
     URL = "http://da-checker-tool2.herokuapp.com/execute"
     requests.get(URL)
+    # <meta http-equiv = "refresh" content = "5; url = https://docs.google.com/spreadsheets/d/1zJAhm3UoSxiF4ne1degW6uUuP2pLxw9rYckmF4jho0M/edit#gid=0" />
     return """
-    <meta http-equiv = "refresh" content = "5; url = https://docs.google.com/spreadsheets/d/1zJAhm3UoSxiF4ne1degW6uUuP2pLxw9rYckmF4jho0M/edit#gid=0" />
     <h1>Execution Started ...<br>
     Please don't Relode or Close this Page <br> 
     It'll take a 3-4 minute to update the Google Sheets if no error Occurs <br><br>
